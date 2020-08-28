414d0af412bd76a79245de266f94e6d060b191c0
Pillamore <jgeekudin@gmail.com>
2020-08-29 05:42:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# piramore.github.io
+Just a personal portofolio, nothing more, nothing less.
+
+## Find Me!
+You can find me on [facebook](https://facebook.com/piramore) and [twitter](https://twitter.com/piramore). Thank you!
---
+++ precache-manifest.055b0cc6c388fadce1688e26b9fb94f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5eb9e15efd9247fec7d74be547bcd92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2277653ab616fa1ff7cb",
+    "url": "/static/css/main.59654227.chunk.css"
+  },
+  {
+    "revision": "20331748577c9829b1be",
+    "url": "/static/js/2.4eb2b2d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4eb2b2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2277653ab616fa1ff7cb",
+    "url": "/static/js/main.f00dd486.chunk.js"
+  },
+  {
+    "revision": "20977dc29667d18028d9",
+    "url": "/static/js/runtime-main.44fcda9c.js"
+  },
+  {
+    "revision": "4c732369e54ee14c3d884b15b441ad0f",
+    "url": "/static/media/angular.4c732369.svg"
+  },
+  {
+    "revision": "240a5d3abdea2d7c3e9e204dbf46140e",
+    "url": "/static/media/bootstrap.240a5d3a.png"
+  },
+  {
+    "revision": "d0109b299ad21029d9405bb59a54fdae",
+    "url": "/static/media/elastic.d0109b29.svg"
+  },
+  {
+    "revision": "b1fe0968f840456a1a6f7879053eb3d4",
+    "url": "/static/media/nodejs.b1fe0968.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/react.638be909.png"
+  }
+]);
